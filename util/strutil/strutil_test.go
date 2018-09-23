71fc253878fdeec70bce83b3fac45480aca94f95
Scott Meeuwsen <3615587+nu11ptr@users.noreply.github.com>
2018-09-23 09:16:24 -0400

    Refactored len tests as table tests

--- util/strutil/strutil_test.go
+++ util/strutil/strutil_test.go
@@ -6,36 +6,26 @@ import (
 )
 
 func TestLen(t *testing.T) {
-	t.Run("Empty", func(t *testing.T) {
-		s := ""
-		l := Len(s)
-		if l != len(s) {
-			t.Fatal("want", len(s), "got", l)
-		}
-	})
-	t.Run("Plain", func(t *testing.T) {
-		s := "abc abc"
-		l := Len(s)
-		if l != len("abc abc") {
-			t.Fatal("want", len("abc abc"), "got", l)
-		}
-	})
-	t.Run("FalsePositive", func(t *testing.T) {
-		s := "abc\x1babc"
-		l := Len(s)
-		if l != len("abc abc") {
-			t.Fatal("want", len("abc abc"), "got", l)
-		}
-	})
-	t.Run("SetAndResetColor", func(t *testing.T) {
-		s := string([]rune{
+	tests := []struct {
+		name, input string
+		output      int
+	}{
+		{"Empty", "", len("")},
+		{"Plain", "abc abc", len("abc abc")},
+		{"FalsePositive", "abc\x1babc", len("abc abc")},
+		{"SetAndResetColor", string([]rune{
 			esc, lBracket, '3', '6', 'm', 'a', 'b', 'c', ' ', 'a', 'b', 'c', esc, lBracket, '0', 'm',
+		}), len("abc abc")},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			l := Len(test.input)
+			if l != test.output {
+				t.Error("want", test.output, "got", l)
+			}
 		})
-		l := Len(s)
-		if l != len("abc abc") {
-			t.Fatal("want", len("abc abc"), "got", l)
-		}
-	})
+	}
 }
 
 func TestTruncate(t *testing.T) {
